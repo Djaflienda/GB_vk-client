9cec93651904e19215a662f335ee4bef94f43343
MacBook-Игорь <djaflienda@gmail.com>
2019-04-06 20:32:07 +0200

    initial commit

---
+++ GB-VK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GB-VK
+//
+//  Created by MacBook-Игорь on 06/04/2019.
+//  Copyright © 2019 MacBook-Игорь. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
