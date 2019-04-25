63f3c8f78f52b22e47bb050b66e5e2dd6e1cd26b
djaflienda <djaflienda@gmail.com>
2019-04-25 15:02:43 +0300

    newsfeed UI

---
+++ GB-VK/Controller/Newsfeed/NewsfeedViewController.swift
@@ -0,0 +1,51 @@
+//
+//  NewsfeedViewController.swift
+//  GB-VK
+//
+//  Created by MacBook-Игорь on 25/04/2019.
+//  Copyright © 2019 MacBook-Игорь. All rights reserved.
+//
+
+import UIKit
+
+class NewsfeedViewController: UIViewController {
+
+    //MARK: -IBOutlet
+    @IBOutlet private weak var tableView: UITableView!
+    
+    //MARK: -Properties
+    
+    //MARK: -Init
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+    }
+    
+    //MARK: -Handlers
+}
+
+extension NewsfeedViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 212.0
+    }
+}
+
+extension NewsfeedViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NewsfeedTableViewCell.reuseID, for: indexPath) as! NewsfeedTableViewCell
+        
+        
+        return cell
+    }
+}
---
+++ GB-VK/View/NewsfeedCell/NewsfeedTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewsfeedTableViewCell.swift
+//  GB-VK
+//
+//  Created by MacBook-Игорь on 25/04/2019.
+//  Copyright © 2019 MacBook-Игорь. All rights reserved.
+//
+
+import UIKit
+
+class NewsfeedTableViewCell: UITableViewCell {
+
+    static let reuseID = "NewsfeedTableViewCell"
+
+}
